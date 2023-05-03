2f9c6ce0f825fb7370922ec7394512f4a7e5be86
Colombani Lorenzo | 5T <lorenzo.colombani.st@DESKTOP-8AELP4R.Buzzi.Aule>
2023-05-03 11:29:13 +0200

    packet tracer wireless e php

--- Informatica/esercitazione php session/index.php
+++ Informatica/esercitazione php session/index.php
@@ -10,7 +10,6 @@
     <title>Document</title>
 </head>
 <body class="container mt-5">
-<form>
     <div class="text-center">
         <h1 class="mb-5">Login</h1>
     </div>
@@ -31,6 +30,11 @@
         <!-- 2 column grid layout for inline styling -->
         <div class="row mb-4">
             <div class="col d-flex justify-content-center">
+                <!-- Submit button -->
+                <input type="submit" class="btn btn-primary btn-block mb-4" value="Sign in">
+            </div>
+
+            <div class="col d-flex justify-content-center">
                 <!-- Checkbox -->
                 <div class="form-check">
                     <input class="form-check-input" type="checkbox" value="" id="form2Example31" checked />
@@ -38,36 +42,11 @@
                 </div>
             </div>
 
-            <div class="col">
-                <!-- Simple link -->
-                <a href="#!">Forgot password?</a>
+            <div class="col d-flex justify-content-center">
+                <!-- Register button -->
+                <p>Not registered yet? <a href="#!">Sign up</a></p>
             </div>
         </div>
-
-        <!-- Submit button -->
-        <input type="submit" class="btn btn-primary btn-block mb-4" value="Sign in">
     </form>
-
-    <!-- Register buttons -->
-    <div class="text-center">
-        <p>Not a member? <a href="#!">Register</a></p>
-        <p>or sign up with:</p>
-        <button type="button" class="btn btn-link btn-floating mx-1">
-            <i class="fab fa-facebook-f"></i>
-        </button>
-
-        <button type="button" class="btn btn-link btn-floating mx-1">
-            <i class="fab fa-google"></i>
-        </button>
-
-        <button type="button" class="btn btn-link btn-floating mx-1">
-            <i class="fab fa-twitter"></i>
-        </button>
-
-        <button type="button" class="btn btn-link btn-floating mx-1">
-            <i class="fab fa-github"></i>
-        </button>
-    </div>
-</form>
 </body>
 </html>
